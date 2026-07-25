74b2cd06e75508ec4143b10e1665bb2ae76aa0f9
Colin <colin.taylor@mail.mcgill.ca>
2026-07-25 12:24:52 -0400

    fixed matching

--- mapfree_refine/matching/matching.py
+++ mapfree_refine/matching/matching.py
@@ -18,23 +18,24 @@ class MAST3RWrapper():
 
         images = load_images([img0_path, img1_path], size=512)
 
-
-        img0_resize_size = np.array(images[0]["true_shape"]).reshape(-1)[-2:]
+        # true_shape is [H, W]; [-2:] -> [H, W], so [0]=H, [1]=W
+        img0_resize_size = np.array()(images[0]["true_shape"]).reshape(-1)[-2:]
         img1_resize_size = np.array(images[1]["true_shape"]).reshape(-1)[-2:]
-        img0_sx, img0_sy = img0_resize_size[0]/img0_size[1], img0_resize_size[1]/img0_size[0]
 
-        img1_sx, img1_sy = img1_resize_size[0]/img1_size[1], img1_resize_size[1]/img1_size[0]
+        # sx = resized_W / orig_W ; sy = resized_H / orig_H
+        img0_sx, img0_sy = img0_resize_size[1]/img0_size[1], img0_resize_size[0]/img0_size[0]
+        img1_sx, img1_sy = img1_resize_size[1]/img1_size[1], img1_resize_size[0]/img1_size[0]
 
         K0_resize = K0.copy()
         K1_resize = K1.copy()
 
-        K0_resize[0][0] *= img0_sx
-        K0_resize[0][2] *= img0_sx
-        K0_resize[1][1] *= img0_sy
-        K0_resize[1][2] *= img0_sy
+        K0_resize[0][0] *= img0_sx   # fx
+        K0_resize[0][2] *= img0_sx   # 'x
+        K0_resize[1][1] *= img0_sy   # fy
+        K0_resize[1][2] *= img0_sy   # cy
 
         K1_resize[0][0] *= img1_sx
-        K1_resize[0][2] *= img1_sx
+        K1_resize[0][0] *= img1_sx
         K1_resize[1][1] *= img1_sy
         K1_resize[1][2] *= img1_sy
 
@@ -46,15 +47,16 @@ class MAST3RWrapper():
         desc1 = pred1['desc'].squeeze(0).detach()
         desc2 = pred2['desc'].squeeze(0).detach()
 
-        matches_im0, matches_im1 = fast_reciprocal_NNs(desc1, desc2, subsample_or_initxy1=8, device=self.device, dist='dot', block_size=2**13)
-        print(matches_im0[:5])
-        print(matches_im0.dtype)
-        pts3d_0 = pred1['pts3d'].squeeze(0).cpu().numpy()       
+        matches_im0, matches_im1 = fast_reciprocal_NNs(
+            desc1, desc2, subsample_or_initxy1=8,
+            device=self.device, dist='dot', block_size=2**13)
+
+        pts3d_0 = pred1['pts3d'].squeeze(0).cpu().numpy()
         pts3d_1 = pred2['pts3d_in_other_view'].squeeze(0).cpu().numpy()
 
         X0 = pts3d_0[matches_im0[:,1], matches_im0[:,0]]
         X1 = pts3d_1[matches_im1[:,1], matches_im1[:,0]]
-
         conf = pred1['conf'].squeeze(0).cpu().numpy()[matches_im0[:,1], matches_im0[:,0]]
-        # mathces and Ks are in resize space
-        return X0, X1, conf, matches_im0, matches_im1 , K0_resize, K1_resize
+
+        # matches and Ks are in 512-resolution (resized) space
+        return X0, X1, conf, matches_im0, matches_im1, K0_resize, K1_resize
